37bbc3c9f38eabaf53489d9978ee184549e6228f
Vladimir Farafonov <vfarafonov@ruswizards.com>
2014-12-08 16:11:09 +0400

    Added change color button

--- src/com/example/rwlab46/MainActivity.java
+++ src/com/example/rwlab46/MainActivity.java
@@ -2,18 +2,33 @@ package com.example.rwlab46;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 
 public class MainActivity extends Activity {
 
-    @Override
+    protected static final String LOG_TAG = "Main Activity";
+
+	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        Button mBtnChangeColor = (Button)findViewById(R.id.buttonChangeColor);
+        mBtnChangeColor.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Log.d(LOG_TAG, "Button pressed");
+			}
+		});
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
